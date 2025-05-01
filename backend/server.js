86b0297ccd90062b4870c345e1562806911a47e0
Alvy <alvyahmed03@gmail.com>
2025-05-02 05:17:21 +0600

    User auth and basic auction done

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan';
+import dotenv from 'dotenv'
+import auctionRoutes from './api/routes/auctionRoutes.js'
+import authRoutes from './api/routes/authRoutes.js'
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/api/auctions', auctionRoutes);
+app.use('/api', authRoutes);
+
+//Routes
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+})
+
+export default app;
