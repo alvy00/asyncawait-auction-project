41b86236a863a5cb7bc4b9f25031fd0c377f4097
Alvy <alvyahmed03@gmail.com>
2025-05-05 21:23:21 +0600

    Added an auction creation page and connected with api

--- backend/api/routes/auctionRoutes.js
+++ backend/api/routes/auctionRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import supabase from '../../config/supabaseClient.js';
 import { auctionSchema } from '../../utils/schema.js';
+import { getUser } from '../controllers/authController.js';
 
 
 const auctionRouter = express.Router();
+const user = await getUser();
 
 // Get all auctions
 auctionRouter.get('/', async (req, res) => {
@@ -23,26 +25,58 @@ auctionRouter.get('/', async (req, res) => {
 
 //Create Auction
 auctionRouter.post('/create', async (req, res) => {
-    try{
+    try {
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
         const result = auctionSchema.safeParse(req.body);
-        if(!result.success){
+        if (!result.success) {
             return res.status(400).json({
                 error: 'Invalid auction data',
                 issues: result.error.issues,
-            })
+            });
+        }
+
+        const { data, error } = await supabase
+            .from('auctions')
+            .insert([
+                {
+                    user_id: user.id,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                    ...result.data,
+                },
+            ]);
+
+        console.log("Supabase Response:", { data, error });
+
+        if (error) {
+            return res.status(500).json({
+                error: 'Database Insert Failed',
+                message: error.message,
+            });
+        }
+
+        if (!data || data.length === 0) {
+            return res.status(500).json({
+                error: 'Database Insert Returned No Data',
+                message: 'Failed to create auction, no data returned.',
+            });
         }
 
         res.status(201).json({
             message: 'Auction created successfully',
-            auction: result.data,
-        })
-    }catch(e){
+            auction: data[0],
+        });
+
+    } catch (e) {
         console.error(e);
         return res.status(500).json({
             error: 'Internal Server Error',
             message: e.message || 'An error occurred while creating the auction',
         });
     }
-})
+});
 
 export default auctionRouter;
--- frontend/app/components/HeroSection.tsx
+++ frontend/app/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { Button } from '@frontend/components/ui/button';
+import { Button } from "../../components/ui/button";
 
 export const HeroSection = () => {
   return (
